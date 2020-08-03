ff7d69de203c1b4d7dfb20c78e30e82cecbd2257
gilphilbert <gil.philbert@gmail.com>
2020-08-02 22:07:07 -0700

    Web update

--- src/main.cpp
+++ src/main.cpp
@@ -258,10 +258,41 @@ void dimScreen() {
 }
 
 void configureServer() {
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
   //configure SPIFFS
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html");
+  //server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  //  request->send(SPIFFS, "/index.html");
+  //});
+  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
+    shouldReboot = !Update.hasError();
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
+    response->addHeader("Connection", "close");
+    request->send(response);
+  },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
+    if(!index){
+      Serial.printf("Update Start: %s\n", filename.c_str());
+      Update.runAsync(true);
+      if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
+        Update.printError(Serial);
+      }
+    }
+    if(!Update.hasError()){
+      if(Update.write(data, len) != len){
+        Update.printError(Serial);
+      }
+    }
+    if(final){
+      if(Update.end(true)){
+        Serial.printf("Update Success: %uB\n", index+len);
+      } else {
+        Update.printError(Serial);
+      }
+    }
   });
+  server.serveStatic("/", SPIFFS, "/www/").setDefaultFile("index.html");
 }
 
 void enableWifi() {
