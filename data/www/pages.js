416956d2ea19fc18205e98cb5b9b131ce1782254
gilphilbert <gil.philbert@gmail.com>
2020-09-07 23:56:49 -0700

    new icons, menu layout

--- data/www/pages.js
+++ data/www/pages.js
@@ -6,10 +6,10 @@ function getContent(fragmentId, callback) {
     }
   }
   var pages = {
-    main: cr.div(
+    main: cr.div({ class: 'menu' },
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
+          //cr.div({ class: 'box' },
             cr.h2('Volume'),
             cr.div({ class: 'row' },
               cr.div({ class: 'col-xs nogrow' },
@@ -21,7 +21,7 @@ function getContent(fragmentId, callback) {
                 )
               )
             )
-          )
+          //)
         )
       ),
       cr.div({ class: 'row' },
@@ -34,7 +34,7 @@ function getContent(fragmentId, callback) {
       ),
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
+          //cr.div({ class: 'box' },
             cr.h2('Settings'),
             cr.div({ class: 'row middle-xs' },
               cr.div({ class: 'col-xs' },
@@ -43,20 +43,20 @@ function getContent(fragmentId, callback) {
                 )
               )
             )
-          )
+          //)
         )
       ),
     ),
-    settings: cr.div(
+    settings: cr.div({ class: 'menu' },
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
+          cr.a({ href: '#inputs' },
             cr.div({ class: 'row middle-xs' },
               cr.div({ class: 'col-xs' },
                 cr.h2('Input names'),
                 cr.p('Your inputs are names, not numbers!')
               ),
-              cr.div({ class: 'col-xs fixed-52' },
+              cr.div({ class: 'col-xs nogrow' },
                 cr.span({ class: 'button-round'}, getSVG('chevron-right'))
               )
             )
@@ -65,66 +65,72 @@ function getContent(fragmentId, callback) {
       ),
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
+          //cr.div({ class: 'box' },
             cr.div({ class: 'row middle-xs' },
               cr.div({ class: 'col-xs' },
                 cr.h2('Dim screen'),
                 cr.p('Screen too bright? Dim automatically after 10s')
               ),
-              cr.div({ class: 'col-xs fixed-52' },
+              cr.div({ class: 'col-xs nogrow' },
                 cr.label({ class: 'switch pure-flex-middle-xs' },
                   cr.input({ type: 'checkbox', id: 'dim-screen' }),
                   cr.span({ class: 'slider round' })
                 )
               )
             )
-          )
+          //)
         )
       ),
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
-            cr.div({ class: 'row middle-xs' },
-              cr.div({ class: 'col-xs' },
-                cr.h2('Maximum volume'),
-                cr.p('Protect your equipment (and your ears) by limiting volume')
-              ),
-              cr.div({ class: 'col-xs fixed-52' },
-                //cr.span({ class: 'button-round'}, getSVG('chevron-right'))
-              )
+          cr.div({ class: 'row middle-xs' },
+            cr.div({ class: 'col-xs' },
+              cr.h2('Maximum volume'),
+              cr.p('Protect your equipment (and your ears) by limiting volume')
+            ),
+            cr.div({ class: 'col-xs nogrow' },
+              //cr.div({ class: 'addon' },
+              //  cr.button(getSVG('minus')),
+              //  cr.input({ type: 'text', id: 'max-volume' }),
+              //  cr.button(getSVG('plus'))
+              //)
+              cr.span({ class: 'button-round'}, getSVG('chevron-right'))
             )
           )
         )
       ),
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
             cr.div({ class: 'row middle-xs' },
               cr.div({ class: 'col-xs' },
                 cr.h2('Startup volume'),
                 cr.p('Keep making yourself jump? Set a maximum startup volume')
               ),
-              cr.div({ class: 'col-xs fixed-52' },
-                //cr.span({ class: 'button-round'}, getSVG('chevron-right'))
+              cr.div({ class: 'col-xs nogrow' },
+                //cr.div({ class: 'addon' },
+                //  cr.button(getSVG('minus')),
+                //  cr.input({ type: 'text', id: 'max-start-volume' }),
+                //  cr.button(getSVG('plus'))
+                //)
+                cr.span({ class: 'button-round'}, getSVG('chevron-right'))
               )
             )
-          )
         )
       ),
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
           cr.a({ href: '#wifi' },
-            cr.div({ class: 'box' },
+            //cr.div({ class: 'box' },
               cr.div({ class: 'row middle-xs' },
                 cr.div({ class: 'col-xs' },
                   cr.h2('Wireless'),
                   cr.p('Connect this device to your Wifi')
                 ),
-                cr.div({ class: 'col-xs fixed-52' },
+                cr.div({ class: 'col-xs nogrow' },
                   cr.span({ class: 'button-round'}, getSVG('chevron-right'))
                 )
               )
-            )
+            //)
           )
         )
       ),
@@ -137,7 +143,7 @@ function getContent(fragmentId, callback) {
                   cr.h2('Firmware'),
                   cr.p('Manage this device\'s firmware')
                 ),
-                cr.div({ class: 'col-xs fixed-52' },
+                cr.div({ class: 'col-xs nogrow' },
                   cr.span({ class: 'button-round'}, getSVG('chevron-right'))
                 )
               )
@@ -146,31 +152,30 @@ function getContent(fragmentId, callback) {
         )
       )
     ),
-    inputs: cr.div(
+    inputs: cr.div({ class: 'menu' },
+      cr.div({ class: 'row' },
+        cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
+          cr.div({ class: 'box' },
+            cr.h2('Inputs'),
+            cr.div({ id: 'input-list' })
+          )
+        )
+      )
+    ),
+    wifi: cr.div({ class: 'menu' },
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
           cr.div({ class: 'box' },
             cr.h2('Select a WiFi network:'),
             cr.div({ id: 'network-list' },
-              cr.p('Scanning...')
+              cr.p('Scanning...'),
+              getSVG('rotate-cw', 'spinner')
             )
           )
         )
       )
     ),
-    wifi: cr.div(
-      cr.div({ class: 'row' },
-        cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
-          cr.div({ class: 'box' },
-            cr.h2('Select a WiFi network:'),
-            cr.div({ id: 'network-list' },
-              cr.p('Scanning...')
-            )
-          )
-        )
-      )
-    ),
-    firmware: cr.div(
+    firmware: cr.div({ class: 'menu' },
       cr.div({ class: 'row' },
         cr.div({ class: 'col-lg-4 col-lg-offset-4 col-xs-12' },
           cr.div({ class: 'box' },
